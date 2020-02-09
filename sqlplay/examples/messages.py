dba8e547f1d8e37a78e454f4441359e52f7a52ce
big-o <big-o@users.noreply.github.com>
2020-02-09 14:32:44 +0000

    sampling bug fix and new example

--- sqlplay/examples/messages.py
+++ sqlplay/examples/messages.py
@@ -5,33 +5,33 @@ _DATERNG = pd.Timedelta(weeks=8)
 
 
 # Tables of sent and received messages.
-# Question: Write a SQL statement to find the success rate for message transmission.
-# Answer: Given that the expected fraction of message IDs in the data is ~0.8
-# expect the answer to be around 0.8**2 == 0.64
+
+# Question: What is the message delivery success rate per day?
 
 nmsg = 100
-ratio = 0.8
+ratio = 0.5
 
 schema = {
     "sent": {
-        "size": int(round(ratio, nmsg)),
+        "size": round(ratio * nmsg),
         "fields": {
-            "msgid": {"values": range(nmsg)},
+            "msgid": {"values": range(nmsg), "repeat": False},
             "timestamp": {
                 "values": pd.date_range(
                     # Ensure send dates are always before receive dates
-                    pd.Timestamp.now() - (_DATERNG) * 2,
+                    pd.Timestamp.now() - (_DATERNG * 2),
                     periods=_DATERNG.total_seconds() // 60,
                     freq="min",
                 ),
                 "sort": True,
+                "repeat": False,
             },
         },
     },
     "received": {
-        "size": int(round(ratio, nmsg)),
+        "size": round(ratio * nmsg),
         "fields": {
-            "msgid": {"values": range(nmsg)},
+            "msgid": {"values": range(nmsg), "repeat": False},
             "timestamp": {
                 "values": pd.date_range(
                     pd.Timestamp.now() - _DATERNG,
@@ -39,6 +39,7 @@ schema = {
                     freq="min",
                 ),
                 "sort": True,
+                "repeat": False,
             },
         },
     },
--- sqlplay/sqlplay/sqlplay.py
+++ sqlplay/sqlplay/sqlplay.py
@@ -2,6 +2,7 @@
 
 import click
 from importlib.util import spec_from_file_location, module_from_spec
+import logging
 from sqlalchemy import create_engine
 import numpy as np
 import pandas as pd
@@ -38,56 +39,20 @@ def make_row(flds):
     return row
 
 
-@click.command()
-@click.option(
-    "-u",
-    "--url",
-    required=True,
-    help="SQL URL to make connection with, e.g. 'mysql://user@localhost'.",
-)
-@click.option("-d", "--db", default="play", help="Database name")
-@click.option(
-    "-e",
-    "--if-exists",
-    default="fail",
-    type=click.Choice(["append", "fail", "replace"]),
-    help="Table name",
-)
-@click.option(
-    "-c",
-    "--config",
-    required=True,
-    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
-    help=(
-        "Optional config file. Should contain dicts of field names -> "
-        "sequence of possible values called `fields'. "
-        "The dict names will be used as table names."
-    ),
-)
-def main(url, db, if_exists, config):
-    spec = spec_from_file_location("config", config)
-    mod = module_from_spec(spec)
-    spec.loader.exec_module(mod)
-    tbls = mod.schema
-
-    for name, schema in tbls.items():
-        schema = schema.copy()
-        schema["fields"] = prepare_fields(schema["fields"])
-        make_table(url, db, if_exists, table=name, **schema)
-
-
 def make_table(url, db, if_exists, table, size, fields):
-    df = pd.DataFrame([make_row(fields) for i in range(size)])
+    logger = logging.getLogger()
+
+    data = [make_row(fields) for i in range(size)]
+    df = pd.DataFrame(data)
+
     # Non-repeat fields weren't added. Add those now.
     for field, schema in fields.items():
         if not schema.get("repeat", True):
-            vals = schema["values"]
+            vals = list(schema["values"])
             if size > len(vals):
+                # Pad it out with a few NULLs
                 vals = vals + [None] * (size - len(vals))
-            col = np.random.choice(vals, replace=False, size=len(vals))
-            if size < len(vals):
-                col = col[:size]
-
+            col = np.random.choice(vals, replace=False, size=size)
             df[field] = col
 
     # Reset the ordering
@@ -116,7 +81,49 @@ def make_table(url, db, if_exists, table, size, fields):
 
     conn.close()
 
-    print(f"Table `{db}:{table}' created successfully.")
+    logger.info(f"Table `{db}:{table}' created successfully.")
+
+
+@click.command()
+@click.option(
+    "-u",
+    "--url",
+    required=True,
+    help="SQL URL to make connection with, e.g. 'mysql://user@localhost'.",
+)
+@click.option("-d", "--db", default="play", help="Database name")
+@click.option(
+    "-e",
+    "--if-exists",
+    default="fail",
+    type=click.Choice(["append", "fail", "replace"]),
+    help="Table name",
+)
+@click.option(
+    "-c",
+    "--config",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+    help=(
+        "Optional config file. Should contain dicts of field names -> "
+        "sequence of possible values called `fields'. "
+        "The dict names will be used as table names."
+    ),
+)
+def main(url, db, if_exists, config):
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s:%(funcName)s: %(message)s"
+    )
+
+    spec = spec_from_file_location("config", config)
+    mod = module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    tbls = mod.schema
+
+    for name, schema in tbls.items():
+        schema = schema.copy()
+        schema["fields"] = prepare_fields(schema["fields"])
+        make_table(url, db, if_exists, table=name, **schema)
 
 
 if __name__ == "__main__":
