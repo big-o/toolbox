92b20bba61a20e230959ea0c8a48c2d87d187ced
big-o <big-o@users.noreply.github.com>
2020-02-17 19:39:04 +0000

    new pkg name

--- datagen/setup.py
+++ datagen/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="datagen",
     version="0.1.0",
-    description="Generate random SQL databases from a schema.",
+    description="Generate random data from a schema.",
     url="http://github.com/big-o/toolbox/sqlplay",
     author="big-o",
     author_email="big-o@github",
