beeab6475372740ef6c9145c44a8717b8a7154cc
big-o <big-o@users.noreply.github.com>
2020-02-17 19:51:30 +0000

    find_packages fix

--- datagen/setup.py
+++ datagen/setup.py
@@ -4,9 +4,9 @@ setup(
     name="datagen",
     version="0.1.0",
     description="Generate random data from a schema.",
-    url="http://github.com/big-o/toolbox/sqlplay",
+    url="http://github.com/big-o/toolbox/datagen",
     author="big-o",
     author_email="big-o@github",
     license="MIT",
-    packages=find_packages(exclude="example.*"),
+    packages=find_packages(exclude="examples"),
 )
